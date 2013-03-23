657ecace1f900ef4aeb61fbce25c976412031f08
Upamanyu Das <upamanyudas@hotmail.com>
2013-03-23 17:44:23 +0530

    Bookmarklet

---
+++ Opera Extention/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function shutup(){var headID=document.getElementsByTagName('head')[0];var cssNode=document.createElement('link');cssNode.type='text/css';cssNode.rel='stylesheet';cssNode.href='http://www.upamanyu.in/butwi.css';headID.appendChild(cssNode);})()
